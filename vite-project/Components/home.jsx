c06868345f236f5de9ecb9de065f620a86f2da8b
Neema_Mwende <gracepete12@gmail.com>
2024-02-26 14:47:26 +0300

    updates

---
+++ vite-project/Components/home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+    return ( 
+        <div className="container">
+            <h2>My Name is <span>Neema Mwende</span></h2>
+            <p>I am a <span>Software Developer</span><br/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Sit dolores quod veritatis error ducimus aliquid nostrum 
+            alias consequatur. Itaque, quibusdam. Exercitationem ad 
+            maiores itaque vitae sunt, praesentium quis distinctio fugiat.
+            </p>
+            <div>
+                <img src="" alt="" />
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;
